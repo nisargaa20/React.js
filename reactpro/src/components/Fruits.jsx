d2509269aa28ac6ac9d731caccc6e2cf9318619b
nisargaa20 <125184228+nisargaa20@users.noreply.github.com>
2024-04-14 16:23:29 +0530

    Rendering Array Of Objects

--- reactpro/src/components/Fruits.jsx
+++ reactpro/src/components/Fruits.jsx
@@ -1,9 +1,22 @@
 export default function Fruits(){
-  const fruits=["Apple","Mango"];
+  //const fruits=["Apple","Mango"];
+
+  const fruits=[
+  { name:"Apple",price:10,emoji:"🍎" },
+  { name:"Mango",price:7,emoji:"🥭"},
+  { name:"Banana",price:6,emoji:"🍌" },
+  { name:"Orange",price:4,emoji:"🍊" },
+  
+  ];
   return (
   <div>
     <ul>
-      {fruits.map(fruit=><li key={fruit}>{fruit}</li>)}
+      {fruits.map((fruit)=>(
+      <li key={fruit.name}>
+        {""}
+        {fruit.emoji} {fruit.name} ${fruit.price}</li>
+    )
+      )}
       </ul>
       </div>
    
