5874e795bcc57ad1288af93984c5a4989b70b752
nisargaa20 <125184228+nisargaa20@users.noreply.github.com>
2024-04-17 07:05:42 +0530

    Using Ternary Operators

--- reactpro/src/components/conditionalComponent.jsx
+++ reactpro/src/components/conditionalComponent.jsx
@@ -2,16 +2,10 @@ import welcome from "./welcome";
 import Code from "./Code";
 
 export default function ConditionalComponent(){
-  let messageOne ;
-  let messagetwo;
-  let message;
+ 
   const display=false;
-  if(display){
-    message=<h1>This is message 1</h1>
-  }else{
-    return <h1>This is message 2</h1>;
-  }
-  return message;
+  return display ? <Welcome /> : <Code />;
+
   }
 
 
